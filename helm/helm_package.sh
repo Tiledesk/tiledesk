a3169d7f81dd536550efce95316e267d37cfe4d5
Andrea Leo <andrea.leo83@gmail.com>
2022-06-29 19:11:13 +0200

    s

--- helm/helm_package.sh
+++ helm/helm_package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-helm package helm -d helm/releases
-helm repo index --url https://tiledesk.github.io/tiledesk-deployment/helm/releases --merge index.yaml ./helm/releases/
+helm package helm -d /releases
+helm repo index --url https://tiledesk.github.io/tiledesk-deployment/helm/releases --merge index.yaml ./releases/
 git push
