a2b9f0c7dea6141d0243888f081fae71cc110ac1
Andrea Leo <andrea.leo83@gmail.com>
2022-09-07 16:42:19 +0200

    helm package

--- helm_package.sh
+++ helm_package.sh
@@ -2,7 +2,7 @@
 
 git pull
 helm package helm -d helm/releases
-helm repo index --url https://tiledesk.github.io/tiledesk-deployment/helm/releases --merge index.yaml ./helm/releases/
+helm repo index --url https://tiledesk.github.io/tiledesk/helm/releases --merge index.yaml ./helm/releases/
 git add .
 git commit -m "helm package" 
 git push
