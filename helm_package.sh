3c3f03bf78dac05f4c1c7c02fb4a985db75563e3
Gabriele Panico <gabriele.panico@gmail.com>
2022-11-09 10:03:46 +0100

    helm package

--- helm_package.sh
+++ helm_package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git pull
-helm package helm -d helm/releases
+# git pull
+# helm package helm -d helm/releases
 helm repo index --url https://tiledesk.github.io/tiledesk/helm/releases --merge index.yaml ./helm/releases/
 git add .
 git commit -m "helm package" 
