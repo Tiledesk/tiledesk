0f26da9ef6cbb9ac9ec5fbf534801b867339f2be
Gabriele Panico <gabriele.panico@gmail.com>
2024-04-19 10:59:13 +0200

    improvements

--- helm_package.sh
+++ helm_package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+helm lint ./helm
 git pull
 helm package helm -d helm/releases
 helm repo index --url https://tiledesk.github.io/tiledesk/helm/releases --merge index.yaml ./helm/releases/
