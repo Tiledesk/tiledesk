97da903d7e9b1a98bb52f1939abfdf5dca3d0a24
Gabriele Panico <gabriele.panico@gmail.com>
2024-04-23 13:05:34 +0200

    added version argument to deploy script

--- helm_package.sh
+++ helm_package.sh
@@ -4,6 +4,6 @@ git pull
 helm package helm -d helm/releases
 helm repo index --url https://tiledesk.github.io/tiledesk/helm/releases --merge index.yaml ./helm/releases/
 git add .
-git commit -m "helm package" 
+git commit -m "helm package Chart:$1" 
 git push
 helm repo update
